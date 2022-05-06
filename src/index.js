29d494f7317bc1d041c5e12a0bf96e86e06c760a
Ritik kumar <thakurritik38921@gmail.com>
2022-05-06 12:50:43 +0530

    dfijnsijfnwign

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import {Provider} from "react-redux";
 import {store,persistor} from "./redux/store";
-import { PersistGate } from 'redux-persist/integration/react'
-
+// import {} from "redux-persist/integrat"
+// import { PersistGate } from 'redux-persist/integration/react';
+import {PersistGate} from "redux-persist/integration/react";
 
 
 ReactDOM.render(
